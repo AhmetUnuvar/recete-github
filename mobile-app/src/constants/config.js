6206166948ed820a5444b1089d0b075b64598229
AhmetUnuvar <95508776+AhmetUnuvar@users.noreply.github.com>
2026-05-19 12:20:56 +0300

    aab_updated

--- mobile-app/src/constants/config.js
+++ mobile-app/src/constants/config.js
@@ -3,6 +3,6 @@
  * EXPO_PUBLIC_API_BASE_URL=http://192.168.x.x:4000
  */
 const raw =
-  process.env.EXPO_PUBLIC_API_BASE_URL || "http://89.167.4.8:4000";
+  process.env.EXPO_PUBLIC_API_BASE_URL || "http://89.167.4.8";
 
 export const API_BASE_URL = raw.replace(/\/$/, "");
